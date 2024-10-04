07ae906f1f867532ccad8a277d784b757b09238d
Michael Lohier <lohiermichael@gmail.com>
2024-10-04 18:05:19 +0200

    Add constants for the commands
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- src/main.py
+++ src/main.py
@@ -17,12 +17,17 @@ daily_question = "What's your ideal date?"
 user_responses = {}
 last_command = {}
 
-HELPER = """
+# Constants for command strings
+COMMAND_ANSWER = "answer"
+COMMAND_CANCEL = "cancel"
+COMMAND_HELP = "help"
+
+HELPER = f"""
 Here are the available commands. Only these can be used to interact with the bot.
 
-/help - open the helper menu
-/answer - answer to to question of the day
-/cancel - cancel the previously started action (like respond to the question of the day)
+/{COMMAND_HELP} - open the helper menu
+/{COMMAND_ANSWER} - answer to to question of the day
+/{COMMAND_CANCEL} - cancel the previously started action (ex: /{COMMAND_ANSWER})
 """
 
 
@@ -62,7 +67,7 @@ async def answer(update: Update, _) -> None:
         parse_mode="MarkdownV2",
     )
 
-    last_command[user_id] = "/answer"
+    last_command[user_id] = COMMAND_ANSWER
 
 
 async def cancel(update: Update, _) -> None:
@@ -103,7 +108,7 @@ async def handle_message(update: Update, _) -> None:
     logger.info(f"User {user.name} input a message")
 
     # Check the last command issued by the user
-    if last_command.get(user.id) != "/answer":
+    if last_command.get(user.id) != COMMAND_ANSWER:
         await update.message.reply_text(HELPER)
         return
 
@@ -118,9 +123,9 @@ def main() -> None:
         logger.error("The bot token is not defined")
         return
     application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler("answer", answer))
-    application.add_handler(CommandHandler("cancel", cancel))
-    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler(COMMAND_ANSWER, answer))
+    application.add_handler(CommandHandler(COMMAND_CANCEL, cancel))
+    application.add_handler(CommandHandler(COMMAND_HELP, help))
 
     application.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
