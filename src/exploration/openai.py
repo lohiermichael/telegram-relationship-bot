a1d7582e5f8978467ba4c25b5eca6fb739449941
Michael Lohier <lohiermichael@gmail.com>
2024-10-04 13:21:36 +0200

    exploration: play withe the OpenAI API
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

---
+++ src/exploration/openai.py
@@ -0,0 +1,37 @@
+import os
+
+from openai import OpenAI
+from dotenv import load_dotenv
+
+from src.logger import setup_logger
+
+load_dotenv()
+
+# Set up logging
+logger = setup_logger()
+
+def main() -> None:
+    # Retrieve the OpenAI API key from environment variables
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+    if not OPENAI_API_KEY:
+        logger.error("OpenAI API key not found in environment variables.")
+        raise ValueError("OpenAI API key is required")
+
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": "Write a short sentence with advice to find love",
+            },
+        ],
+    )
+
+    print(completion.choices[0].message)
+
+if __name__ == "__main__":
+    main()
