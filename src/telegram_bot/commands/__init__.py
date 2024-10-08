454b8b0d93f8c68089d42aca11b70dc4ebf92e43
Michael Lohier <lohiermichael@gmail.com>
2024-10-08 23:11:50 +0200

    telegram_bot/commands: add /start to HELPER
    Signed-off-by: Michael Lohier <lohiermichael@gmail.com>

--- src/telegram_bot/commands/__init__.py
+++ src/telegram_bot/commands/__init__.py
@@ -9,6 +9,7 @@ COMMAND_START = "start"
 HELPER = f"""
 Here are the available commands. Only these can be used to interact with the bot.
 
+/{COMMAND_START} - start interacting with the bot
 /{COMMAND_HELP} - open the helper menu
 /{COMMAND_ANSWER} - answer to the question of the day
 /{COMMAND_CANCEL} - cancel the previously started action (ex: /{COMMAND_ANSWER})
